b38c49e5b4ee9ddf3b727cc5da5f7ca3fb113143
Feeloh <philipmwendwa3@gmail.com>
2019-03-01 15:48:51 +0300

    migration with default data

--- application/migrations/20190301142505_create_bank.php
+++ application/migrations/20190301142505_create_bank.php
@@ -2,35 +2,27 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Migration_Create_employer extends CI_Migration {
+class Migration_Create_bank extends CI_Migration {
 
         public function up()
         {
                 $this->dbforge->add_field(array(
-                        'employer_id' => array(
-                                'type' => 'INT',
-                                'constraint' => 11,
-                                'unsigned' => TRUE,
-                                'auto_increment' => TRUE
+                        'bank_id' => array(
+                            'type' => 'INT',
+                            'constraint' => 11,
+                            'unsigned' => TRUE,
+                            'auto_increment' => TRUE
                         ),
-                        'employer_name' => array(
+                        'bank_name' => array(
                                 'type' => 'VARCHAR',
                                 'constraint' => '100',
                         ),
-                        'employer_email' => array(
+                        'bank_code' => array(
                                 'type' => 'VARCHAR',
                                 'constraint' => '100',
-                        ),
-                        'employer_phone_number' => array(
-                            'type' => 'VARCHAR',
-                            'constraint' => '100',
-                        ),
-                        'employer_postal_address' => array(
-                            'type' => 'VARCHAR',
-                            'constraint' => '100',
                         ),
                         ));
-                    $this->dbforge->add_field("`employer_status` tinyint NOT NULL DEFAULT 1");
+                    $this->dbforge->add_field("`bank_status` tinyint NOT NULL DEFAULT 1");
                     $this->dbforge->add_field("`created_by` int NOT NULL ");
                     $this->dbforge->add_field("`created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
                     $this->dbforge->add_field("`modified_by` int NULL ");
@@ -38,12 +30,19 @@ class Migration_Create_employer extends CI_Migration {
                     $this->dbforge->add_field("`deleted_by` int NULL");
                     $this->dbforge->add_field("`deleted` tinyint NOT NULL DEFAULT 0");
                     $this->dbforge->add_field("`deleted_on` timestamp NULL DEFAULT NULL");
-                    $this->dbforge->add_key('employer_id', TRUE);
-                    $this->dbforge->create_table('employer');
+                    $this->dbforge->add_key('bank_id', TRUE);
+                    $this->dbforge->create_table('bank');
+                    $data = array(
+                            array('bank_id'=> "1",
+                                    'bank_name' => "Central Bank"),
+                                array('bank_id'=>"2",
+                                'bank_name' => "Barclays Bank")
+                        );
+                   $this->db->insert_batch('bank', $data);
         }
 
         public function down()
         {
-                $this->dbforge->drop_table('employer');
+                $this->dbforge->drop_table('bank');
         }
 }
