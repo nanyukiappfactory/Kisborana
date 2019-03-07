c4e1ff39640845085e169621777972ba7d3f0724
ndutagrace25 <ndutagrace25@gmail.com>
2019-03-07 11:50:00 +0300

    created a fumction to check member existence

--- application/config/routes.php
+++ application/config/routes.php
@@ -110,7 +110,7 @@ $route['members/deactivate/(:num)'] = 'microfinance/members/deactivate/$1';
 $route['members/activate/(:num)'] = 'microfinance/members/activate/$1';
 $route['members/gitdelete/(:num)'] = 'microfinance/members/delete_member/$1';
 $route['members/execute_search'] = 'microfinance/members/execute_search';
-$route['members/check-member-existence'] = 'microfinance/members/check_member_existence';
+$route['members/check-member-existence/(:num)'] = 'microfinance/members/check_member_existence/$phone';
 
 //saving_types routes
 $route['saving_types'] = 'microfinance/saving_types/index';
