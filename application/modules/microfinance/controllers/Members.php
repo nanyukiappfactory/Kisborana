101ca17fcacdff6db69897730ee2c1f69cb11984
unknown <caroline.theuri@nanyukiappfactory.onmicrosoft.com>
2019-03-11 07:14:08 +0300

    adding national id parameter

--- application/modules/microfinance/controllers/Members.php
+++ application/modules/microfinance/controllers/Members.php
@@ -250,9 +250,9 @@ public function check_member_existence($phone, $nationalid)
         if($all_members->num_rows() > 0)
         {
             $insert_member_phone_number = $this->member_model->insert_phone_number($phone, $nationalid);
-            var_dump($insert_member_phone_number);die();
             if($insert_member_phone_number == true){
                 $members = $all_members->result();
+                var_dump($members);die();
                 $members_encoded = json_encode($members);
                 echo $members_encoded;
             }
