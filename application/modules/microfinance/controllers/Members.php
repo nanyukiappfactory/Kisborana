b9debb6157f786e59878a8105b9073188b164f9d
unknown <caroline.theuri@nanyukiappfactory.onmicrosoft.com>
2019-03-12 17:24:16 +0300

    kaizala updates

--- application/modules/microfinance/controllers/Members.php
+++ application/modules/microfinance/controllers/Members.php
@@ -277,14 +277,14 @@ class Members extends MX_Controller
     }
 
     //get members to create web serrvice
-    public function check_member_existence($phone, $nationalid)
+    public function check_member_existence($phone, $nationalid,$payroll_number)
     {
        
-        $all_members = $this->member_model->check_member_existence($phone, $nationalid);
+        $all_members = $this->member_model->check_member_existence($phone, $nationalid,$payroll_number);
 
         if($all_members->num_rows() > 0)
         {
-            $insert_member_phone_number = $this->member_model->insert_phone_number($phone, $nationalid);
+            $insert_member_phone_number = $this->member_model->insert_phone_number($phone, $nationalid,$payroll_number);
             if($insert_member_phone_number == true){
                 $members = $all_members->result();
                 //var_dump($members);die();
