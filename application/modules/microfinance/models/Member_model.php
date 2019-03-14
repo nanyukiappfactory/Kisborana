4f0eb1b2b3f9e6b7a42242a6105d28f424d20e39
ndutagrace25 <ndutagrace25@gmail.com>
2019-03-14 17:19:23 +0300

    end points created

--- application/modules/microfinance/models/Member_model.php
+++ application/modules/microfinance/models/Member_model.php
@@ -291,14 +291,29 @@ class Member_model extends CI_Model
             'member_password'=> md5($password),
             'member_phone_number' => $phone_number
         );
-        
-        $this->db->where('member_national_id', $nationalid);
-        if($this->db->update("member",$data)){
+
+        //============== trial=======
+            $this->db->where('member_national_id', $nationalid);
+            $query_string = $this->db->insert_string('member', $data);
+            $query_string = str_replace('INSERT INTO', 'INSERT IGNORE INTO', $query_string);
+
+            $req = $this->db->query($query_string);
+
+            if($req->affected_rows() == 1) {
             return TRUE;
-        }
-        else{
+            } else {
+            //email exists already
             return FALSE;
-        }
+            }
+        //============== end ========
+        
+        // $this->db->where('member_national_id', $nationalid);
+        // if($this->db->update("member",$data)){
+        //     return TRUE;
+        // }
+        // else{
+        //     return FALSE;
+        // }
     }
 
     public function get_total_members()
