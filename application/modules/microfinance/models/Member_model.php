62795cd0696ae802934d962892f1af3efe6b5ea9
Feeloh <philipmwendwa3@gmail.com>
2019-03-14 18:20:19 +0300

    end points for phone numbers created

--- application/modules/microfinance/models/Member_model.php
+++ application/modules/microfinance/models/Member_model.php
@@ -291,29 +291,23 @@ class Member_model extends CI_Model
             'member_password'=> md5($password),
             'member_phone_number' => $phone_number
         );
-
-        //============== trial=======
-            $this->db->where('member_national_id', $nationalid);
-            $query_string = $this->db->insert_string('member', $data);
-            $query_string = str_replace('INSERT INTO', 'INSERT IGNORE INTO', $query_string);
-
-            $req = $this->db->query($query_string);
-
-            if($req->affected_rows() == 1) {
+        $this->db->where('member_national_id', $nationalid);
+        if($this->db->insert("member",$data)){
             return TRUE;
-            } else {
-            //email exists already
+        }
+        else{
             return FALSE;
-            }
-        //============== end ========
-        
-        // $this->db->where('member_national_id', $nationalid);
-        // if($this->db->update("member",$data)){
-        //     return TRUE;
-        // }
-        // else{
-        //     return FALSE;
-        // }
+        }
+    }
+    function retrieve_phone($phone_number)
+    {       
+        $data = array(
+            'member_phone_number' => $phone_number
+        );
+        $this->db->select('member_phone_number');    
+        $this->db->where($data);
+        $member_details = $this->db->get("member");
+        return $member_details;
     }
 
     public function get_total_members()
